f84544862fe6a73ab4a75f3a636633897c97b914
Ambrose Blay <ambrose2002blay@gmail.com>
2024-01-25 20:43:53 -0500

    clerk authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
+ 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignedOut>
+        <SignInButton />
+        <p>This content is public. Only signed out users can see the SignInButton above this text.</p>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton afterSignOutUrl="/" />
+        <p>This content is private. Only signed in users can see the SignOutButton above this text.</p>
+      </SignedIn>
     </div>
-  );
+  )
 }
-
-export default App;
+ 
+export default App
